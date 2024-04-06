9b7c97bada74041ba9b1e1917d0b910f41dc7504
RiturajAgrahari <agr.rituraj@gmail.com>
2024-04-06 17:07:56 +0530

    uploading the project with login frontend

---
+++ src/components/authentication/signup.tsx
@@ -0,0 +1,56 @@
+import { get } from "http";
+import "./signup.css"
+import { useState } from "react";
+
+const SignUp = () => {
+
+    const [switchSide, setSwitchSide] = useState(true)
+
+    const switch_side = () => {
+        console.log("switching side")
+
+        setSwitchSide(!switchSide)
+
+        
+
+
+
+    }
+
+    return (
+        <div className="authentication_page">
+            <div className="authentication_form">
+                <div className="signin" style={{opacity: switchSide ? 1 : 0, visibility: switchSide ? "visible" : "hidden", transform: switchSide ? "translateX(0%)" : "translateX(25%)"}}>
+                    <h2>Sign in</h2>
+                    <input className="auth_form_input" type="email" placeholder="Email"></input>
+                    <input className="auth_form_input" type="password" placeholder="Password"></input>
+                    <a>Forgot your password?</a>
+                    <button className="auth_button">Sign in</button>
+                </div>
+                <div className="signup" style={{opacity: !switchSide ? 1 : 0, visibility: !switchSide ? "visible" : "hidden", transform: !switchSide ? "translateX(100%)" : "translateX(75%)"}}>
+                    <h2>Create Account</h2>
+                    <input className="auth_form_input" type="text" placeholder="Full Name"></input>
+                    <input className="auth_form_input" type="email" placeholder="Email"></input>
+                    <input className="auth_form_input" type="password" placeholder="Password"></input>
+                    <input className="auth_form_input" type="password" placeholder="Confirm Password"></input>
+                    <button className="auth_button">Sign up</button>
+                </div>
+                <div className="auth_content" style={{left: switchSide ? "50%" : 0, borderRadius: switchSide ? "0 20px 20px 0": "20px 0 0 20px"}}>
+                </div>
+                <div className="auth_content_text" style={{opacity: switchSide ? 1 : 0, visibility: switchSide ? "visible" : "hidden", transform: switchSide ? "translateX(100%)" : "translateX(130%)"}}>
+                        <h2 className="auth_switch_heading">Hello, Friend!</h2>
+                        <p>Enter your personal details and start journey with us</p>
+                        <button className="auth_switch_button" onClick={switch_side}>Sign up</button>
+                </div>
+                <div className="auth_content_text" style={{opacity: !switchSide ? 1 : 0, visibility: !switchSide ? "visible" : "hidden", transform: !switchSide ? "translateX(0%)" : "translateX(-30%)"}}>
+                        <h2 className="auth_switch_heading">Welcome Back!</h2>
+                        <p>To keep connected with us please login with your personal info</p>
+                        <button className="auth_switch_button" onClick={switch_side}>Sign in</button>
+                </div>
+            </div>
+        </div>
+    )
+ 
+};
+
+export default SignUp;
